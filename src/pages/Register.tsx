3c4d4c9bb5f5c09ebd959efafc65059d1052f5c1
Lorena <yiselorenasalzargmail.com>
2025-07-17 18:28:25 -0500

    cambios

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,21 +1,31 @@
 import { useState } from "react";
 import { userUser } from "../UserContext";
 
-const Register:React.FC=()=>{
-    const [email,setEmail]=useState('')
-    const [password,setPassword]=useState('')
-    const {register}=userUser()
-    const guardar= async ()=>{
-        await register(email,password)
-    }
-return(
-<div>
-    <form>
-        <input type="email" onChange={(event)=>setEmail(event.target.value)}></input>
-         <input type="password" onChange={(event)=>setPassword(event.target.value)}></input>
-         <button type="submit"></button>
-    </form>
-</div>
-)
-}
+const Register: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { register } = userUser();
+
+  const guardar = async (event: React.FormEvent) => {
+    event.preventDefault(); 
+    await register(email, password);
+  };
+
+  return (
+    <div>
+      <form onSubmit={guardar}>
+        <input 
+          type="email" 
+          onChange={(event) => setEmail(event.target.value)} 
+        />
+        <input 
+          type="password" 
+          onChange={(event) => setPassword(event.target.value)} 
+        />
+        <button type="submit" >Registrarse</button>
+      </form>
+    </div>
+  );
+};
+
 export default Register;
